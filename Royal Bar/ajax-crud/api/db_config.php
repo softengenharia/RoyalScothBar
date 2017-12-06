d5ae13b7f9f0298a2ad5918264be58778616bae6
Aluno IFMG-Campus Bambuí <aluno@LAB01-DSKX>
2017-12-06 14:07:59 +0000

    Quarta versão

--- Royal Bar/ajax-crud/api/db_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	define (DB_USER, "php");
-	define (DB_PASSWORD, "test");
-	define (DB_DATABASE, "h_blog");
-	define (DB_HOST, "localhost");
-	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
-?>
